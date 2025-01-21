3c8154555e34316031d9f160ce1a2dfe1f686369
10ash <10ashimovasanali10@gmail.com>
2025-01-21 15:08:06 +0500

    practice

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Practice App!');
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
